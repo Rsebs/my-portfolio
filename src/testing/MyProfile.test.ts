1ba423649f04b20571317f9a8091c324457c94be
Rsebs <sebastianruizj2014@gmail.com>
2026-05-18 00:42:09 -0500

    Se agrega configuración básica para test con vitest

---
+++ src/testing/MyProfile.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import vuetify from '@/plugins/vuetify'
+import MyProfile from '@/views/MyProfile.vue'
+
+const createWrapper = () => mount(MyProfile, { global: { plugins: [vuetify] } })
+
+describe('MyProfile', () => {
+  // it = individual test case
+  it('renders the component correctly', () => {
+    const wrapper = createWrapper()
+    expect(wrapper.find('h1').text()).toBe('Sebastián Ruiz J.')
+  })
+
+  it('renders contact chips', () => {
+    const wrapper = createWrapper()
+    expect(wrapper.findAll('.v-chip')).toHaveLength(4)
+    expect(wrapper.findAll('.v-chip')[0]?.text()).toBe('sebastiantianruizj2014@gmail.com')
+    expect(wrapper.findAll('.v-chip')[1]?.find('.mdi-file-document').exists()).toBe(true)
+    expect(wrapper.findAll('.v-chip')[2]?.find('.mdi-linkedin').exists()).toBe(true)
+    expect(wrapper.findAll('.v-chip')[3]?.find('.mdi-github').exists()).toBe(true)
+  })
+})
